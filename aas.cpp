2191cc2d301851f3f2a98e6928ef5034c3e99004
alifndaru <alif@DESKTOP-TCAL917.(none)>
2021-12-19 12:40:47 +0700

    backup devC++

---
+++ aas.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout <<"MENGHITUNG HARGA TOMAT & MELON"<<endl;
+    int tomat = 9000;
+    int melon = 10000;
+    int harga;
+    harga = tomat + melon;
+    cout<<"harga nya adalah Rp"<<harga<<endl;
+    
+
+
+
+}
